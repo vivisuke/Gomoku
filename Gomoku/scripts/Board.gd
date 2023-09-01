ef5e7fefd0c95c1d18254d3ef0259fe1e4ba8636
vivisuke <ntsuda@master.email.ne.jp>
2023-09-02 05:19:22 +0900

    0007 ◎ C 09/01 09/02 0.001│ToDo│座標ラベル設置

--- Gomoku/scripts/Board.gd
+++ Gomoku/scripts/Board.gd
@@ -1,4 +1,4 @@
-extends ColorRect
+﻿extends ColorRect
 
 const BD_WIDTH = 500
 const N_HORZ = 11
@@ -6,8 +6,8 @@ const N_VERT = 11
 const FRAME_WD = 38
 #const CELL_WD = (BD_WIDTH - FRAME_WD) / N_HORZ
 const CELL_WD = 42		# 42*12 = 504
-const ORG_X = 42		# 原点
-const ORG_Y = 42		# 原点
+const ORG_X = 42+10		# 原点
+const ORG_Y = 42+10		# 原点
 const RT_END = ORG_X+CELL_WD*(N_HORZ-1)
 const LW_END = ORG_Y+CELL_WD*(N_VERT-1)
 
@@ -22,6 +22,18 @@ func _process(delta):
 	pass
 
 func _draw():
+	for x in range(N_HORZ):
+		var l = Label.new()
+		l.text = "abcdefghijk"[x]
+		l.set_position(Vector2(ORG_X+CELL_WD*x-4, -1))
+		l.add_theme_color_override("font_color", Color.BLACK)
+		add_child(l)
+	for y in range(N_VERT):
+		var l = Label.new()
+		l.text = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"][y]
+		l.set_position(Vector2(2, ORG_Y+CELL_WD*y-10))
+		l.add_theme_color_override("font_color", Color.BLACK)
+		add_child(l)
 	for x in range(N_HORZ):
 		var wd = 2.5 if x == 0 || x == N_HORZ - 1 else 1
 		draw_line(Vector2(ORG_X, ORG_Y+CELL_WD*x),
