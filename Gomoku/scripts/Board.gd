624b147baf0f631803b1d56c69377b7cc3b199b5
vivisuke <ntsuda@master.email.ne.jp>
2023-09-02 10:48:12 +0900

    0014 ◎ C 09/01 09/02 0.001│ToDo│盤面影描画

--- Gomoku/scripts/Board.gd
+++ Gomoku/scripts/Board.gd
@@ -34,6 +34,12 @@ func _process(delta):
 	pass
 
 func _draw():
+	var style_box = StyleBoxFlat.new()      # 影、ボーダなどを描画するための矩形スタイルオブジェクト
+	style_box.bg_color = color   # 矩形背景色
+	style_box.shadow_offset = Vector2(0, 4)     # 影オフセット
+	style_box.shadow_size = 8                   # 影（ぼかし）サイズ
+	draw_style_box(style_box, Rect2(Vector2(0, 0), self.size))      # style_box に設定した矩形を描画
+	#
 	for x in range(N_HORZ):
 		var wd = 2.5 if x == 0 || x == N_HORZ - 1 else 1
 		draw_line(Vector2(ORG_X, ORG_Y+CELL_WD*x),
