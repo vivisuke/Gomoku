51ca67e5725642554948d3debbc0382eb988930b
vivisuke <ntsuda@master.email.ne.jp>
2023-09-02 10:45:27 +0900

    0015 ◎ C 09/01 09/02 0.001│ToDo│座標ラベル生成は _ready() に移動

--- Gomoku/scripts/Board.gd
+++ Gomoku/scripts/Board.gd
@@ -13,15 +13,7 @@ const LW_END = CELL_WD*(N_VERT-0.5)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("CELL_WD = ", CELL_WD)
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func _draw():
+	#print("CELL_WD = ", CELL_WD)
 	for x in range(N_HORZ):
 		var l = Label.new()
 		l.text = "abcdefghijk"[x]
@@ -34,6 +26,14 @@ func _draw():
 		l.set_position(Vector2(-26, ORG_Y+CELL_WD*y-10))
 		l.add_theme_color_override("font_color", Color.BLACK)
 		add_child(l)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _draw():
 	for x in range(N_HORZ):
 		var wd = 2.5 if x == 0 || x == N_HORZ - 1 else 1
 		draw_line(Vector2(ORG_X, ORG_Y+CELL_WD*x),
