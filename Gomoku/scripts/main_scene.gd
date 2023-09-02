a67ca50c2d1b6cadc561c6756c38add67de2d01a
vivisuke <ntsuda@master.email.ne.jp>
2023-09-03 07:08:28 +0900

    0027 ◎ C 09/03 09/03 0.001│ToDo│着手箇所強調 0028 ◎ C 09/03 09/03 0.001│ToDo│次手番のみアンダーライン表示

--- Gomoku/scripts/main_scene.gd
+++ Gomoku/scripts/main_scene.gd
@@ -12,11 +12,13 @@ var bd
 var rng = RandomNumberGenerator.new()
 var AI_thinking = false
 var waiting = 0;				# ウェイト中カウンタ
-var game_started = false		# ゲーム中か？
+var game_started = true			# ゲーム中か？
 var next_color = g.BLACK		# 次の手番
 #var white_player = HUMAN
 #var black_player = HUMAN
 var pressedPos = Vector2(0, 0)
+var put_pos = Vector2(-1, -1)
+var prev_put_pos = Vector2(-1, -1)
 
 func _ready():
 	#rng.randomize()		# Setups a time-based seed
@@ -27,19 +29,28 @@ func _ready():
 	bd = g.Board.new()
 	#bd.put_color(5, 5, g.BLACK)
 	#bd.put_color(6, 5, g.WHITE)
-	update_board()
+	update_view()
 	pass # Replace with function body.
 
-func update_board():
+func update_view():
+	update_next_underline()
 	for y in range(N_VERT):
 		for x in range(N_HORZ):
 			var c:int = bd.get_color(x, y)
 			$Board/TileMap.set_cell(0, Vector2(x, y), c-1, Vector2i(0, 0))
+	if prev_put_pos.x >= 0:
+		$Board/BGTileMap.set_cell(0, prev_put_pos, -1, Vector2i(0, 0))
+	if put_pos.x >= 0:
+		$Board/BGTileMap.set_cell(0, put_pos, 2, Vector2i(0, 0))
+		print("put_pos = ", put_pos)
 	pass
+func update_next_underline():
+	$WhitePlayer/Underline.visible = game_started && next_color == g.WHITE
+	$BlackPlayer/Underline.visible = game_started && next_color == g.BLACK
 func _process(delta):
 	pass
 func _input(event):
-	#if !game_started: return
+	if !game_started: return
 	if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT:
 		#print(event.position)
 		#print($Board/TileMapLocal.local_to_map(event.position - BOARD_ORG))
@@ -59,5 +70,7 @@ func _input(event):
 			#print(pos)
 			bd.put_color(pos.x, pos.y, next_color)
 			next_color = (g.BLACK + g.WHITE) - next_color
-			update_board()
+			prev_put_pos = put_pos
+			put_pos = pos
+			update_view()
 	pass
