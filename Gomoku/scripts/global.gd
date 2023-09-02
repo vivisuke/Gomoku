d251914e0f35aeb78bdbddf5e39dab0e23d96666
vivisuke <ntsuda@master.email.ne.jp>
2023-09-02 13:14:52 +0900

    0019 ◎ C 09/02 09/02 0.001│ToDo│Board クラス追加 0020 ◎ C 09/02 09/02 0.001│ToDo│タイトル・バージョンラベルを分ける

--- Gomoku/scripts/global.gd
+++ Gomoku/scripts/global.gd
@@ -3,6 +3,16 @@ extends Node2D
 const N_HORZ = 11
 const N_VERT = 11
 
+class Board:
+	var hbitmap = []		# 水平方向ビットマップ
+	var vbitmap = []		# 垂直方向ビットマップ
+	func _init():
+		hbitmap.resize(N_VERT)
+		hbitmap.fill(0)
+		vbitmap.resize(N_HORZ)
+		vbitmap.fill(0)
+
+
 func _ready():
 	pass # Replace with function body.
 func _process(delta):
